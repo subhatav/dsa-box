8f8ecf9f129339a4b7989d8f7c8b317df4cb959a
Subhatav Dhali <subhatav@gmail.com>
2023-01-24 18:06:53 +0530

    Add the Stocks Trading problem under the Arrays section

---
+++ src/arrays/TradeStocks.java
@@ -0,0 +1,22 @@
+package arrays;
+
+// LeetCode Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// Approach #1: Brute Force; Time Complexity: O(N^2)
+// Approach #2: Traverse & Check; Time Complexity: O(N)
+
+public class TradeStocks {
+
+    public int compute(int[] prices) {
+
+        int profit = 0, buy = prices[0];
+
+        for (int price : prices) {
+
+            if (price > buy) profit = Math.max(profit, price - buy);
+            else if (price < buy) buy = price;
+        }
+
+        return profit;
+    }
+}
