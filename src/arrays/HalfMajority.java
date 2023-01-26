356ae6fde71a47efd37322ac3b6e2737f4c90463
Subhatav Dhali <subhatav@gmail.com>
2023-01-26 19:41:25 +0530

    Add the Half Majority Element problem under the Arrays section

---
+++ src/arrays/HalfMajority.java
@@ -0,0 +1,32 @@
+package arrays;
+
+// LeetCode Link: https://leetcode.com/problems/majority-element/
+// Solution Link: https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
+
+// Approach #1: Brute Force; Time Complexity: O(N^2)
+// Approach #2: Sorting & Finding Middle Element; Time Complexity: O(NlogN)
+// Approach #3: Insert in HashMap & Check; Time Complexity: O(N)
+// Approach #4: Boyer-Moore Majority Voting Algorithm; Time Complexity: O(N)
+
+public class HalfMajority {
+
+    public int compute(int[] nums) {
+
+        int count = 0, major = 0;
+
+        for (int num : nums) {
+
+            // Since the Majority Element WILL occur more than (N / 2) times,
+            // there is guaranteed chance that the Majority Element will occur
+            // in AT LEAST two Consecutive Indices in the Array provided; so
+            // the value of the Frequency Variable will become more than 1.
+
+            if (count == 0) major = num;
+            
+            if (num == major) count += 1;
+            else count -= 1;
+        }
+
+        return major;
+    }
+}
