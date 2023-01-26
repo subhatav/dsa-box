118429150ebb2867c77c73992e39f9126a8db4a7
Subhatav Dhali <subhatav@gmail.com>
2023-01-26 17:29:59 +0530

    Add the Search 2D Matrix problem under the Arrays section

---
+++ src/arrays/SearchMatrix.java
@@ -0,0 +1,27 @@
+package arrays;
+
+import java.util.Arrays;
+
+// LeetCode Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+// Approach #1: Brute Force; Time Complexity: O(M * N)
+// Approach #2: Binary Search for entire Matrix; Time Complexity: O(log(M * N))
+// Approach #3: Binary Search for a single Row; Time Complexity: O(M + logN)
+
+public class SearchMatrix {
+
+    public boolean compute(int[][] matrix, int target) {
+
+        int capacity = matrix[0].length;
+
+        for (int[] row : matrix) {
+
+            if (target <= row[capacity - 1]) {
+
+                return Arrays.binarySearch(row, target) < 0 ? false : true;
+            }
+        }
+
+        return false;
+    }
+}
