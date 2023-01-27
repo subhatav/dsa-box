006c462550883cd47386bcc666fccc018b4f2944
Subhatav Dhali <subhatav@gmail.com>
2023-01-27 23:12:25 +0530

    Add the Maximums of K-Sized Windows problem under the Arrays section

---
+++ src/arrays/KSizedWindows.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// LeetCode Link: https://leetcode.com/problems/sliding-window-maximum/
+// Solution Link: https://www.youtube.com/watch?v=DfljaUwZsOk
+
+// Approach #1: Brute Force; Time Complexity: O(N^2)
+// Approach #2: Sliding Window with Deque; Time Complexity: O(N)
+
+public class KSizedWindows {
+
+    public int[] compute(int[] nums, int k) {
+
+        int size = nums.length, j = 0;
+        int[] maxes = new int[size - k + 1];
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < size; i++) {
+
+            // Remove left-most numbers which are OUT of range
+            if (!deque.isEmpty() && deque.peekFirst() == i - k)
+                deque.pollFirst();
+
+            // Remove left-most numbers which are SMALLER than "nums[i]"
+            while (!deque.isEmpty() && nums[i] > nums[deque.peekLast()])
+                deque.pollLast();
+
+            deque.offer(i); // Insert index of "nums[i]" at end
+
+            // If range has "k" width, take the left-most number
+            if (i >= k - 1) maxes[j++] = nums[deque.peekFirst()];
+        }
+        
+        return maxes;
+    }
+}
