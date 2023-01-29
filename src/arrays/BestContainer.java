70b9815feb60fa5b44b95de8751ef078a4e77b85
Subhatav Dhali <subhatav@gmail.com>
2023-01-29 14:58:42 +0530

    Add the Container with Most Water problem under the Arrays section

---
+++ src/arrays/BestContainer.java
@@ -0,0 +1,45 @@
+package arrays;
+
+// LeetCode Link: https://leetcode.com/problems/container-with-most-water/
+// Solution Link: https://www.youtube.com/watch?v=UuiTKBwPgAo
+
+// Approach #1: Brute Force; Time Complexity: O(N^2)
+// Approach #2: Min-Max with 2-Pointers; Time Complexity: O(N)
+
+public class BestContainer {
+
+    public int compute(int[] heights) {
+
+        int maxArea = 0, leftBar = 0, rightBar = heights.length - 1;
+
+        while (leftBar < rightBar) {
+
+            int currLeft = heights[leftBar], currRight = heights[rightBar];
+
+            // Determine the Current Area from the Distance between the Bars,
+            // multiplied with the Minimum Height of the Left and Right Bars
+            int currArea = (rightBar - leftBar) * Math.min(currLeft, currRight);
+
+            maxArea = Math.max(maxArea, currArea);
+
+            if (currLeft < currRight) {
+
+                // Move "leftBar" FORWARD, when Future Height is SMALLER
+                while (leftBar < rightBar && currLeft >= heights[leftBar]) {
+
+                    leftBar += 1;
+                }
+
+            } else {
+
+                // Move "rightBar" BACKWARD, when Future Height is SMALLER
+                while (leftBar < rightBar && currRight >= heights[rightBar]) {
+
+                    rightBar -= 1;
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
--- src/arrays/UniquePaths.java
+++ src/arrays/UniquePaths.java
@@ -8,26 +8,27 @@ package arrays;
 
 public class UniquePaths {
 
-    public int compute(int m, int n) {
+    public int compute2(int m, int n) {
 
-        /*
-            int[][] dp = new int[m][n];
+        int[][] dp = new int[m][n];
 
-            for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
 
-                for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) {
 
-                    if (i == 0 || j == 0) dp[i][j] = 1;
-                    else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
+                if (i == 0 || j == 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
+        }
 
-            return dp[m - 1][n - 1];
-        */
+        return dp[m - 1][n - 1];
+    }
+
+    public int compute3(int m, int n) {
 
         return nCr(m + n - 2, n - 1);
     }
-    
+
     private int nCr(int n, int r) {
 
         double res = 1;
