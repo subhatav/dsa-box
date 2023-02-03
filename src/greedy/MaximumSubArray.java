a5f47fcb623927cd585beabe2a3673879b97a106
Subhatav Dhali <subhatav@gmail.com>
2023-02-04 00:16:24 +0530

    Relocate the Maximum Sub-Array Sum problem under the Greedy section

--- src/greedy/MaximumSubArray.java
+++ src/greedy/MaximumSubArray.java
@@ -1,6 +1,7 @@
-package arrays;
+package greedy;
 
 // Problem Link: https://leetcode.com/problems/maximum-subarray/
+// Solution Link: https://www.youtube.com/watch?v=5WZl3MMT0Eg
 
 // Approach #1: Brute Force; Time Complexity: O(N^3) or O(N^2)
 // Approach #2: Kadane's Algorithm; Time Complexity: O(N)
@@ -16,7 +17,8 @@ public class MaximumSubArray {
             sum += num;
 
             if (sum > max) max = sum;
-            // Reset current "sum" to ZERO, when it goes negative
+            // Reset the current "sum" = ZERO,
+            // when it becomes LESS than ZERO
             if (sum < 0) sum = 0;
         }
 
