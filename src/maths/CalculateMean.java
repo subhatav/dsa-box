a308b756f10088fc13b2bf843ae673aa3965c5cc
Subhatav Dhali <subhatav@gmail.com>
2023-02-26 16:15:39 +0530

    Add the Calculate Mean problem under the Maths section

---
+++ src/maths/CalculateMean.java
@@ -0,0 +1,18 @@
+package maths;
+
+// Approach Link: https://stackoverflow.com/questions/1930454/what-is-a-good-solution-for-calculating-an-average-where-the-sum-of-all-values-e
+
+public class CalculateMean {
+
+    public double compute(int[] numbers) {
+
+        double mean = 0, count = 0;
+
+        for (int number : numbers) {
+
+            mean += (number - mean) / (count += 1);
+        }
+
+        return mean;
+    }
+}
