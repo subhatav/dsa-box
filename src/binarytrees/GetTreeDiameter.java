dd6ef252f35c05f3893718b70184531832bf4c99
Subhatav Dhali <subhatav@gmail.com>
2023-03-01 21:16:16 +0530

    Add the Diameter of Binary Tree problem under the BinaryTrees section

---
+++ src/binarytrees/GetTreeDiameter.java
@@ -0,0 +1,38 @@
+package binarytrees;
+
+import binarytrees.CustomBinaryTree.TreeNode;
+
+// Problem Link: https://leetcode.com/problems/diameter-of-binary-tree/
+// Solution Link: https://takeuforward.org/data-structure/calculate-the-diameter-of-a-binary-tree/
+
+// Approach: Calculate the Maximum Sum of Heights of both Sub-Trees
+// Time Complexity: O(N); Space Complexity: O(H) in Recursion Space
+
+public class GetTreeDiameter {
+
+    private int answer = 0;
+
+    public int compute(TreeNode root) {
+
+        height(root);
+
+        return answer;
+    }
+
+    private int height(TreeNode root) {
+
+        if (root == null) return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        // Garner CURRENT Diameter
+        int current = left + right;
+
+        // MAXIMIZE the Diameter deposited
+        answer = Math.max(answer, current);
+
+        // Return HEIGHT of CURRENT Tree
+        return Math.max(left, right) + 1;
+    }
+}
