9787e224f53a82f10b02e59337b46617fb39e39e
Subhatav Dhali <subhatav@gmail.com>
2023-03-01 20:43:53 +0530

    Add the AIO-Go Tree Traversals problem under the BinaryTrees section

---
+++ src/binarytrees/AllOrderTraversals.java
@@ -0,0 +1,87 @@
+package binarytrees;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import binarytrees.CustomBinaryTree.TreeNode;
+
+// Problem Link: https://www.codingninjas.com/codestudio/problems/981269
+
+// Solution Link: https://takeuforward.org/data-structure/preorder-inorder-postorder-traversals-in-one-traversal/
+
+// Approach: All-in-1-Go Orders Traversal Method
+// Time Complexity: O(N); Space Complexity: O(N)
+
+public class AllOrderTraversals {
+
+    static class Pair {
+
+        int count = 0;
+
+        TreeNode node = null;
+
+        public Pair(int count, TreeNode node) {
+
+            this.count = count;
+
+            this.node = node;
+        }
+    }
+
+    private List<Integer> ins, pres, posts;
+
+    private List<List<Integer>> orders;
+
+    public List<List<Integer>> compute(TreeNode root) {
+
+        Stack<Pair> nodes = new Stack<>();
+
+        ins = new LinkedList<>();
+        pres = new LinkedList<>();
+        posts = new LinkedList<>();
+
+        orders = new LinkedList<>();
+
+        if (root == null) return orders;
+
+        nodes.push(new Pair(1, root));
+
+        while (!nodes.isEmpty()) {
+
+            Pair visited = nodes.pop();
+
+            if (visited.count == 1) {
+
+                pres.add(visited.node.value);
+
+                visited.count += 1;
+                nodes.push(visited);
+
+                if (visited.node.left != null) {
+
+                    nodes.push(new Pair(1, visited.node.left));
+                }
+
+            } else if (visited.count == 2) {
+
+                ins.add(visited.node.value);
+
+                visited.count += 1;
+                nodes.push(visited);
+
+                if (visited.node.right != null) {
+
+                    nodes.push(new Pair(1, visited.node.right));
+                }
+
+            } else posts.add(visited.node.value);
+        }
+
+        orders.add(ins);
+        orders.add(pres);
+        orders.add(posts);
+
+        return orders;
+    }
+}
